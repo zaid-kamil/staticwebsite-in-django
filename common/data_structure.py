9fce4ddfe671e5ff0aba15e69375a375300743e4
zaidkamil <xaidmetamorphos@gmail.com>
2023-11-01 18:21:38 +0530

    content

---
+++ common/data_structure.py
@@ -0,0 +1,10 @@
+name = 'Zaid Kamil'
+print(name[0])
+print(name[2])
+print(name[5])
+print(name[-1])
+# print(name[60])
+
+print(name[:4])
+print(name[5:])
+print(name[3::-1])
---
+++ common/loops.py
@@ -0,0 +1,22 @@
+movies=[
+'Mad Max: Fury Road (2015)',
+'Mission: Impossible - Fallout (2018)',
+'John Wick: Chapter 3 - Parabellum (2019)',
+'Avengers: Endgame (2019)',
+'Extraction (2020)',
+'The Old Guard (2020)',
+'Tenet (2020)',
+'The Gentlemen (2019)',
+'Bad Boys for Life (2020)',
+'Fast & Furious Presents: Hobbs & Shaw (2019)',
+'The Raid 2 (2014)',
+'Kingsman: The Secret Service (2014)',
+'Baby Driver (2017)',
+'Deadpool 2 (2018)',
+'The Equalizer 2 (2018)',
+]
+
+print(f'Total movies = {len(movies)}')
+
+for name in movies:
+    print(name)
